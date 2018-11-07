9d5ea39840a0cf6fcfa09bc24181d3639fdc9b60
Bobby Jenrow <bobbyjenrow@gmail.com>
2018-11-07 10:22:22 -0500

    Added Readme for sass src. Cleaned things up

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -104,18 +104,18 @@ eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./node
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("var normalize = __webpack_require__(/*! normalize.css */ \"./node_modules/normalize.css/normalize.css\");\n\nvar styles = __webpack_require__(/*! ../sass/main.scss */ \"./src/sass/main.scss\");\n\n//# sourceURL=webpack:///./src/js/index.js?");
+eval("var normalize = __webpack_require__(/*! normalize.css */ \"./node_modules/normalize.css/normalize.css\");\n\nvar styles = __webpack_require__(/*! ../scss/main.scss */ \"./src/scss/main.scss\");\n\n//# sourceURL=webpack:///./src/js/index.js?");
 
 /***/ }),
 
-/***/ "./src/sass/main.scss":
+/***/ "./src/scss/main.scss":
 /*!****************************!*\
-  !*** ./src/sass/main.scss ***!
+  !*** ./src/scss/main.scss ***!
   \****************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/sass/main.scss?");
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/scss/main.scss?");
 
 /***/ })
 
